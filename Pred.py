bfd2aa66b5c230ddcc6c5462c6d8730bca7999ea
MoidPardesi <48202597+MoidPardesi@users.noreply.github.com>
2024-01-01 17:58:28 +0000

    Update Pred.py

--- Pred.py
+++ Pred.py
@@ -29,17 +29,16 @@ def main():
     # getting the input data from the user
    
     headline = st.text_input('Headline')
-written_by = st.text_input('Written by')
-news = st.text_input('News article text')
+    written_by = st.text_input('Written by')
+    news = st.text_input('News article text')
 
-# code for Prediction
-prediction_message = ''
+    # code for Prediction
+    prediction_message = ''
 
-# creating a button for Prediction
-if st.button('News Prediction Result'):
-    prediction_message = news_prediction(headline, written_by, news)
-
-st.success(prediction_message)
+    # creating a button for Prediction
+    if st.button('News Prediction Result'):
+        prediction_message = news_prediction(headline, written_by, news)
+        st.success(prediction_message)
 
 if __name__ == '__main__':
     main()
