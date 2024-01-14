6fa1c57a580bf5a8fa2ef121c1ba2824fa4514d6
MoidPardesi <48202597+MoidPardesi@users.noreply.github.com>
2024-01-14 15:55:03 +0000

    Update Pred.py

--- Pred.py
+++ Pred.py
@@ -2,19 +2,14 @@ import numpy as np
 import joblib
 import streamlit as st
 
-# loading the saved model
+
 loaded_model = joblib.load('logistic_regression_model.sav')
 loaded_vectorizer = joblib.load('count_vectorizer.sav')
 
-# creating a function for Prediction
+
 def news_prediction(headline, written_by, news):
-    # Combine the features into a single string
     combined_features = f"{headline} {written_by} {news}"
-    
-    # Transform the input data using the loaded vectorizer
     transformed_input = loaded_vectorizer.transform([combined_features])
-    
-    # Make a prediction
     prediction = loaded_model.predict(transformed_input)
     
     if prediction[0] == 0:
@@ -23,20 +18,20 @@ def news_prediction(headline, written_by, news):
         return 'This news is real'
 
 def main():
-    # giving a title
-    st.title('Fake News Prediction Web App')
     
-    # getting the input data from the user
+    st.title('Fake News Prediction Web App')
     headline = st.text_input('Headline')
     written_by = st.text_input('Written by')
     news = st.text_area('News article text')  # Changed to text_area for longer input
 
-    # code for Prediction
+   
     prediction_message = ''
 
-    # creating a button for Prediction
     if st.button('News Prediction Result'):
-        prediction_message = news_prediction(headline, written_by, news)
+        if headline and written_by and news:
+            prediction_message = news_prediction(headline, written_by, news)
+        else :
+            st.error("Please fill in all the fields to get the prediction.")
 
     st.success(prediction_message)
 
